d88bb1e4ec1e539098087ccc786ccaad4c4ee7d8
CamilaFre <camila.frech@hotmail.com>
2025-03-17 20:12:02 -0300

    exercicio 20

---
+++ ex20.py
@@ -0,0 +1,10 @@
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite um número: "))
+n3 = float(input("Digite um número: "))
+
+lista = [n1,n2,n3]
+
+maximo = max(lista)
+minimo = min(lista)
+
+print(f"Da lista {lista} o valor mínimo é {minimo} e o máximo é {maximo}.")
