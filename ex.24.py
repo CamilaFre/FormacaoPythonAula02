dcc3bfec2e40a5eafc9097b8cb8efc2089457917
CamilaFre <camila.frech@hotmail.com>
2025-03-26 20:11:01 -0300

    exercico 24

---
+++ ex.24.py
@@ -0,0 +1,10 @@
+##Crie uma lista de nomes de alguns animais (pelo menos 5 nomes). Substitua o terceiro item por outro animal, 
+## adicione mais dois animais ao final e, por fim, remova o primeiro elemento da lista.
+
+animais = ["coruja", "morcego", "lobo", "raposa","beluga"]
+
+animais[2] = "urso"
+animais.append("dragão")
+animais.append("polvo")
+
+print(animais)
