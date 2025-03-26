2f5e280b339830544c2014e71b6f280907ec81df
CamilaFre <camila.frech@hotmail.com>
2025-03-26 20:38:30 -0300

    exercicio 26

---
+++ ex.26.py
@@ -0,0 +1,12 @@
+##Crie um set contendo alguns modelos de carro. Tente adicionar um modelo duplicado, 
+# exiba o set e, em seguida, remova um modelo específico.
+
+conjunto = {"fusca", "compass", "amarock"}
+
+conjunto.add("fusca")
+
+print(conjunto)
+
+conjunto.remove("compass")
+
+print(conjunto)
